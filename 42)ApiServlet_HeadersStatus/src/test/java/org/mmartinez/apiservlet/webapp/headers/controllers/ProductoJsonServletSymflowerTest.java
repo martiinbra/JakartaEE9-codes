982f0adbe53f4aacbb56ae90285dbbaead89f950
Martin Martinez Bravo <54995790+martiinbra@users.noreply.github.com>
2023-02-07 12:51:08 -0500

    Add files via upload

---
+++ 42)ApiServlet_HeadersStatus/src/test/java/org/mmartinez/apiservlet/webapp/headers/controllers/ProductoJsonServletSymflowerTest.java
@@ -0,0 +1,25 @@
+package org.mmartinez.apiservlet.webapp.headers.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.junit.jupiter.api.*;
+
+public class ProductoJsonServletSymflowerTest {
+	@Test
+	public void doGet1() throws ServletException, IOException {
+		ProductoJsonServlet p = null; // TODO This is a fallback value due to incomplete analysis.
+		HttpServletRequest req = null; // TODO This is a fallback value due to incomplete analysis.
+		HttpServletResponse resp = null; // TODO This is a fallback value due to incomplete analysis.
+		p.doGet(req, resp);
+	}
+
+	@Test
+	public void doPost2() throws ServletException, IOException {
+		ProductoJsonServlet p = null; // TODO This is a fallback value due to incomplete analysis.
+		HttpServletRequest req = null; // TODO This is a fallback value due to incomplete analysis.
+		HttpServletResponse resp = null; // TODO This is a fallback value due to incomplete analysis.
+		p.doPost(req, resp);
+	}
+}
